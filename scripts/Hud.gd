259ec1abd11a901be4e21097eaf97dc62a83955b
Gumball Studios <gumballstudios@gmail.com>
2017-09-15 07:29:44 -0400

    Hud
    Removed unused process function

--- scripts/Hud.gd
+++ scripts/Hud.gd
@@ -51,23 +51,3 @@ func set_lives(value):
 		lives_container.add_child(life_icon)
 		# HBoxContainer will handle spacing the nodes in the container
 
-
-func _process(delta):
-	var score_text = get_node("ScoreContainer/Score")
-	score_text.set_text(str(score))
-	
-	var lives_container = get_node("LivesContainer")
-	var display_lives = lives - 1
-	if display_lives < 0:
-		return
-	
-	if lives_container.get_child_count() != display_lives:
-		while lives_container.get_child_count() > display_lives:
-			var life_icon = lives_container.get_child(0)
-			lives_container.remove_child(life_icon)
-			life_icon.queue_free()
-		
-		while lives_container.get_child_count() < display_lives:
-			var life_icon = TextureFrame.new()
-			life_icon.set_texture(ball_sprite)
-			lives_container.add_child(life_icon)
